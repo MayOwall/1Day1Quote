6b26e86a21369ded909ee772b0fddd76ec9c6e0c
Owall <97934878+MayOwall@users.noreply.github.com>
2023-05-25 23:16:02 +0900

    feat : 이미지 도메인 설정

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     });
     return config;
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
