a6121845ac6fac9900c679a7d56089942427ab3f
Owall <97934878+MayOwall@users.noreply.github.com>
2023-05-26 14:24:45 +0900

    modify : 컴포넌트 export 및 interface 구현

--- type/index.d.ts
+++ type/index.d.ts
@@ -2,4 +2,58 @@ interface IGNBProps {
   isAuth: boolean;
 }
 
-export {IGNBProps}
+interface IRoundButtonProps {
+  isSelected: boolean;
+  children: string;
+  handleSelect: (v: boolean) => void;
+}
+
+interface ICommunityBtnListProps {
+  btnListData: {
+    selectedBtn: "recent" | "weekly" | "whole";
+    handleSelectedBtn: (selectedBtn: "recent" | "weekly" | "whole") => void;
+  };
+}
+
+interface ICommunityCardHeaderProps {
+  userData: {
+    _id: string;
+    name: string;
+    portrait: string;
+  };
+}
+
+interface ICommunityCardBodyProps {
+  bodyData: {
+    contentData: {
+      _id: string;
+      date: string;
+      image?: string;
+      quote: string;
+      speaker: string;
+      isFired: boolean;
+      fireCount: number;
+      isBookMarked: boolean;
+    };
+    handleFire: (_id: string) => void;
+    handleBookmark: (_id: string) => void;
+  };
+}
+
+interface ICommunityCardProps
+  extends ICommunityCardHeaderProps,
+    ICommunityCardBodyProps {}
+
+interface ICommunityTemplateProps extends ICommunityBtnListProps {
+  cardListData: ICommunityCardProps[];
+}
+
+export {
+  IGNBProps,
+  IRoundButtonProps,
+  ICommunityCardHeaderProps,
+  ICommunityCardBodyProps,
+  ICommunityCardProps,
+  ICommunityBtnListProps,
+  ICommunityTemplateProps,
+};
