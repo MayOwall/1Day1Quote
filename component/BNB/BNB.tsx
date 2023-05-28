e53942c7cb106a67725dddd66c153d67f9b0aae1
Owall <97934878+MayOwall@users.noreply.github.com>
2023-05-28 21:02:17 +0900

    modify : BNB CSS 및 라우팅 로직 수정

--- component/BNB/BNB.tsx
+++ component/BNB/BNB.tsx
@@ -5,21 +5,19 @@ import * as S from "./BNB.styles";
 
 function BNB() {
   const router = useRouter();
-  const isMain = !usePathname().includes("community");
-  const isLoginPage = usePathname().includes("/login");
+  const isWritePage = usePathname().includes("write");
+  const isLoginPage = usePathname().includes("login");
 
   // 버튼 인터렉션과 현재 페이지에 따른 페이지 이동 핸들러
-  const handleBNBBtns = (icon: "main" | "community") => {
+  const handleBNBBtns = (icon: "write" | "community") => {
     // 현재 메인 페이지가 아니면서, 메인 페이지로 요청 이동하면 메인 페이지로 이동
-    if (icon === "main" && !isMain) {
-      router.push("/");
+    if (icon === "write" && !isWritePage) {
+      router.push("/write");
       return;
     }
     // 현재 커뮤니티 페이지가 아니면서, 커뮤니티로 요청 이동하면 커뮤니티 페이지로 이동
-    if (icon === "community" && isMain) {
-      console.log(isMain);
-
-      router.push("/community");
+    if (icon === "community" && isWritePage) {
+      router.push("/");
       return;
     }
   };
@@ -32,15 +30,15 @@ function BNB() {
             <WriteIcon
               width="22px"
               height="23px"
-              fill={isMain ? "#FF4264" : "black"}
-              onClick={() => handleBNBBtns("main")}
+              fill={isWritePage ? "#FF4264" : "black"}
+              onClick={() => handleBNBBtns("write")}
             />
           </S.Btn>
           <S.Btn>
             <CommunityIcon
               width="29px"
               height="24px"
-              fill={!isMain ? "#FF4264" : "black"}
+              fill={!isWritePage ? "#FF4264" : "black"}
               onClick={() => handleBNBBtns("community")}
             />
           </S.Btn>
